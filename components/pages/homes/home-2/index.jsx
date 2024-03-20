03839a92a387d5f19aed1342023191480355a5bb
Sajol2022 <sajolsarker5789@gmail.com>
2024-03-20 11:42:08 +0600

    details fix

--- components/pages/homes/home-2/index.jsx
+++ components/pages/homes/home-2/index.jsx
@@ -25,7 +25,7 @@ const HomeTwo = ({addClass}) => {
             <Video />
             <Work />
             <Consulting />
-            {/* <Portfolio /> */}
+            <Portfolio />
             <Team />
             <Blog />
             <FooterTwo />
