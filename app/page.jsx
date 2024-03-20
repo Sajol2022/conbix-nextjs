dff6a45bcdf9e4e6104471c0a6d4d39160f6b5eb
Sajol2022 <sajolsarker5789@gmail.com>
2024-03-20 10:09:04 +0600

    first commit2

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// import HomeOne from "@/components/pages/homes/home";
+
+export default function Home() {
+    return (
+        <>
+            {/* <HomeOne /> */}
+            <h1>Hello Conbix</h1>
+        </>
+    );
+}
