76e843ed1242d2cdef95ea743394687789b8eb2a
Sajol2022 <sajolsarker5789@gmail.com>
2024-03-20 11:34:02 +0600

    details

--- app/portfolio/[id]/page.jsx
+++ app/portfolio/[id]/page.jsx
@@ -1,21 +1,11 @@
-"use client"
-import { useParams, useRouter } from "next/navigation";
-import portfolioData from "@/components/data/portfolio-data";
+import React from 'react';
 
-const PortfolioDetail = () => {
-    const params = useParams();
-    const portfolioDetails = portfolioData?.find((portfolio) => portfolio.id === params.id);
-    const router = useRouter();
-    if (!portfolioDetails) {
-        return router.push("/404-error");
-    }
+const PortfolioPage = () => {
     return (
         <>
-            {/* <portfoliosSingle portfolioDetails={portfolioDetails} />
-             */}
-             <h1>{portfolioDetails?.title}</h1>
+            
         </>
     );
 };
 
-export default PortfolioDetail;
+export default PortfolioPage;
